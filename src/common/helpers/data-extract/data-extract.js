79644f6a6a38aa351fb7055b5829575080095fd5
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-05-15 10:55:27 +0100

    Specify answer data types

--- src/common/helpers/data-extract/data-extract.js
+++ src/common/helpers/data-extract/data-extract.js
@@ -1,8 +1,13 @@
 /**
- * @typedef  { 'name' | 'file' | 'text' |' radio' | 'address' | 'checkbox' } AnswerType
- * @typedef  {{ type: AnswerType, value: string, displayText: string }} AnswerData
- * @typedef  {{ question: string, questionKey: string, answer: AnswerData }} QuestionAnswerData
- * @typedef  {{ title: string, sectionKey: string, questionAnswers: QuestionAnswerData[]}} SectionData
+ * @typedef {{ type: 'text', value: string, displayText: string }} TextAnswer
+ * @typedef {{ type: 'radio', value: string, displayText: string }} RadioAnswer
+ * @typedef {{ type: 'radio', value: string[], displayText: string }} CheckboxAnswer
+ * @typedef {{ type: 'name', value: { firstName: string, lastName: string }, displayText: string }} NameAnswer
+ * @typedef {{ type: 'address', value: { addressLine1: string, addressLine2?: string, addressTown: string, addressCounty?: string, addressPostcode: string }, displayText: string }} AddressAnswer
+ * @typedef {{ type: 'file', value: { skipped: boolean, path?: string }, displayText: string }} FileAnswer
+ * @typedef { TextAnswer | RadioAnswer | CheckboxAnswer | NameAnswer | AddressAnswer | FileAnswer } AnswerData
+ * @typedef {{ question: string, questionKey: string, answer: AnswerData }} QuestionAnswerData
+ * @typedef {{ title: string, sectionKey: string, questionAnswers: QuestionAnswerData[]}} SectionData
  */
 
 /**
