70708b76d089f1e5fb30bde8acb03903c6efc281
Hugh FD Jackson <hugh@hughfdjackson.com>
2025-05-15 12:10:08 +0100

    DSFAAP-1146: give failAction applicable types

--- src/common/helpers/fail-action.js
+++ src/common/helpers/fail-action.js
@@ -1,8 +1,12 @@
 import { createLogger } from './logging/logger.js'
 
+/** @import { Lifecycle } from '@hapi/hapi' */
+
 const logger = createLogger()
 
-export function failAction(_request, _h, error) {
+/** @type {Lifecycle.FailAction} */
+/** @satisfies {function} */
+export const failAction = (_request, _h, error) => {
   logger.warn(error, error?.message)
   throw error
 }
