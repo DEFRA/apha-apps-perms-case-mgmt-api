e699012721dfe2f46ee746cb8e974a1ea47cba17
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-05-22 14:00:04 +0100

    Fix broken test

--- src/routes/submit/submit-validation.test.js
+++ src/routes/submit/submit-validation.test.js
@@ -11,7 +11,7 @@ const emailAddressQuestion = {
   question: 'Email address',
   questionKey: 'emailAddress',
   answer: {
-    type: 'email',
+    type: 'text',
     value: testEmailAddress,
     displayText: testEmailAddress
   }
