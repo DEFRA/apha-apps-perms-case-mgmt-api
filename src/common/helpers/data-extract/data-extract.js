0c16938f5658f35bedea67c5763f3083cdef8855
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-05-30 14:30:17 +0100

    Added date type

--- src/common/helpers/data-extract/data-extract.js
+++ src/common/helpers/data-extract/data-extract.js
@@ -5,6 +5,7 @@
  * @typedef {{ type: 'name', value: { firstName: string, lastName: string }, displayText: string }} NameAnswer
  * @typedef {{ type: 'address', value: { addressLine1: string, addressLine2?: string, addressTown: string, addressCounty?: string, addressPostcode: string }, displayText: string }} AddressAnswer
  * @typedef {{ type: 'file', value: { skipped: boolean, path?: string }, displayText: string }} FileAnswer
+ * @typedef {{ type: 'date', value: { day: string, month: string, year: string }, displayText: string }} DateAnswer
  * @typedef { TextAnswer | RadioAnswer | CheckboxAnswer | NameAnswer | AddressAnswer | FileAnswer } AnswerData
  * @typedef {{ question: string, questionKey: string, answer: AnswerData }} QuestionAnswerData
  * @typedef {{ title: string, sectionKey: string, questionAnswers: QuestionAnswerData[]}} SectionData
