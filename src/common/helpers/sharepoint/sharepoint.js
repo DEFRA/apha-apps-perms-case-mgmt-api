4c3d36c3d74f75acae6649d6f909776599ac63eb
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-06-13 09:33:26 +0100

    BAU: create scaffolding to allow two tickets to be worked on at once

--- src/common/helpers/sharepoint/sharepoint.js
+++ src/common/helpers/sharepoint/sharepoint.js
@@ -5,6 +5,7 @@ import { statusCodes } from '../../constants/status-codes.js'
 import { getFileProps } from '../email-content/email-content.js'
 import { fetchFile } from '../file/file-utils.js'
 import { sendEmailToApplicant } from '../../connectors/notify/notify.js'
+import { createSharepointItem } from './sharepoint-item.js'
 
 /**
  * @import {FileAnswer} from '../../../common/helpers/data-extract/data-extract.js'
@@ -63,11 +64,16 @@ export const sharePointApplicationHandler = async (request, reference) => {
     }
   }
 
-  await sendEmails(request, reference)
+  await sendApplicantConfirmationEmail(request, reference)
+  await createSharepointItem(request.payload, reference)
+  await sendCaseworkerNotificationEmail(request.payload, reference)
+
   return undefined
 }
 
-const sendEmails = async (request, reference) => {
+const sendCaseworkerNotificationEmail = async (application, reference) => {}
+
+const sendApplicantConfirmationEmail = async (request, reference) => {
   const applicantEmail = getQuestionFromSections(
     'emailAddress',
     'licence',
