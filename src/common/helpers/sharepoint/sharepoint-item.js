154945573bdcbb3aa5142a4a724bc284f7995bfb
Hugh F D Jackson <hugh@hughfdjackson.com>
2025-06-13 15:15:41 +0100

    DSFAAP-1223: add a subset of the required fields for movements off

--- src/common/helpers/sharepoint/sharepoint-item.js
+++ src/common/helpers/sharepoint/sharepoint-item.js
@@ -1,8 +1,36 @@
 import { addItem } from '../../connectors/sharepoint/sharepoint.js'
+import { getQuestionFromSections } from '../data-extract/data-extract.js'
 
+/**
+ * @param {import('../data-extract/data-extract.js').ApplicationData} application
+ * @param {string} reference
+ */
 export const createSharepointItem = async (application, reference) => {
-  const fields = {
+  await addItem(fields(application, reference))
+}
+
+/**
+ * @param {import('../data-extract/data-extract.js').ApplicationData} application
+ * @param {string} reference
+ */
+export const fields = (application, reference) => {
+  const address =
+    /** @type {import('../data-extract/data-extract.js').AddressAnswer} */ (
+      getQuestionFromSections('address', 'origin', application.sections)?.answer
+    )
+
+  const name =
+    /** @type {import('../data-extract/data-extract.js').NameAnswer} */ (
+      getQuestionFromSections('fullName', 'licence', application.sections)
+        ?.answer
+    )
+
+  return {
+    ApplicationSubmittedby: 'Owner/Keeper - Origin',
+    Office: 'Polwhele',
+    MethodofReceipt: 'Digital',
+    FirstlineofAddress: address?.value.addressLine1,
+    Name: name?.displayText,
     Application_x0020_Reference_x002: reference
   }
-  return addItem(fields)
 }
--- src/common/helpers/sharepoint/sharepoint-item.test.js
+++ src/common/helpers/sharepoint/sharepoint-item.test.js
@@ -1,5 +1,8 @@
-import { createSharepointItem } from './sharepoint-item.js'
+import { createSharepointItem, fields } from './sharepoint-item.js'
 import * as sharepoint from '../../connectors/sharepoint/sharepoint.js'
+
+const application = { sections: [] }
+const reference = 'TB-1234-ABCD'
 
 describe('createSharepointItem', () => {
   const mockAddItem = jest
@@ -8,14 +11,101 @@ describe('createSharepointItem', () => {
 
   afterAll(jest.restoreAllMocks)
 
-  it('should set the application reference number correctly', async () => {
-    const application = { sections: [] }
-    const reference = 'TB-1234-ABCD'
-
+  it('should pass through the expected fields correctly', async () => {
     await createSharepointItem(application, reference)
 
-    expect(mockAddItem).toHaveBeenCalledWith({
-      Application_x0020_Reference_x002: reference
+    expect(mockAddItem).toHaveBeenCalledWith(fields(application, reference))
+  })
+
+  it('should propagate errors from the sharepoint connector', async () => {
+    const error = new Error('Item creation failed')
+    mockAddItem.mockRejectedValue(error)
+
+    expect(createSharepointItem(application, reference)).rejects.toThrow(
+      'Item creation failed'
+    )
+  })
+})
+
+describe('fields', () => {
+  const cphNumber = '12/123/1234'
+  const firstName = 'Bob'
+  const lastName = 'Barry'
+  const addressLine1 = '1 the road'
+  const addressTown = 'Townhamlet'
+  const addressPostcode = 'AA10 1AA'
+
+  /** @type {import('../data-extract/data-extract.js').SectionData} */
+  const onFarmOrigin = {
+    sectionKey: 'origin',
+    title: 'Movement origin',
+    questionAnswers: [
+      {
+        question: 'Are the animals moving off of or on to the premises?',
+        questionKey: 'onOffFarm',
+        answer: {
+          type: 'radio',
+          value: 'off',
+          displayText: 'On to the farm or premises'
+        }
+      },
+      {
+        question: 'What is the CPH number of the farm?',
+        questionKey: 'cphNumber',
+        answer: {
+          type: 'text',
+          value: cphNumber,
+          displayText: cphNumber
+        }
+      },
+      {
+        question: 'What is the address of the origin farm?',
+        questionKey: 'address',
+        answer: {
+          type: 'address',
+          value: {
+            addressLine1,
+            addressTown,
+            addressPostcode
+          },
+          displayText: [addressLine1, addressTown, addressPostcode].join('\n')
+        }
+      }
+    ]
+  }
+
+  /** @type {import('../data-extract/data-extract.js').SectionData} */
+  const licence = {
+    sectionKey: 'licence',
+    title: 'Receiving the licence',
+    questionAnswers: [
+      {
+        question: 'What is the full name of the keeper of the animals?',
+        questionKey: 'fullName',
+        answer: {
+          type: 'name',
+          value: {
+            firstName,
+            lastName
+          },
+          displayText: `${firstName} ${lastName}`
+        }
+      }
+    ]
+  }
+  /** @type {import('../data-extract/data-extract.js').ApplicationData} */
+  const application = {
+    sections: [onFarmOrigin, licence]
+  }
+
+  it('should construct expected fields for off the farm', () => {
+    expect(fields(application, reference)).toEqual({
+      Application_x0020_Reference_x002: reference,
+      Office: 'Polwhele',
+      MethodofReceipt: 'Digital',
+      ApplicationSubmittedby: 'Owner/Keeper - Origin',
+      Name: `${firstName} ${lastName}`,
+      FirstlineofAddress: addressLine1
     })
   })
 })
