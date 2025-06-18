9955661f6ecb3e5ad9c278e7ed33c4f44a7fe1a4
Jose Luis Garcia Ramos <11478086+joseluisgraa@users.noreply.github.com>
2025-06-18 16:51:50 +0100

    Fix one test

--- src/common/helpers/start-server.test.js
+++ src/common/helpers/start-server.test.js
@@ -21,6 +21,9 @@ jest.mock('./logging/logger.js', () => ({
     error: (...args) => mockLoggerError(...args)
   })
 }))
+jest.mock('./path-utils.js', () => ({
+  srcFolder: '/mock/src'
+}))
 
 describe('#startServer', () => {
   const PROCESS_ENV = process.env
